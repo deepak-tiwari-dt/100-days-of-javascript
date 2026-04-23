bb98b828592b844fdf23d24ca5000da029129f38
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2026-04-24 00:58:16 +0530

    4th

---
+++ problem4.js
@@ -0,0 +1,15 @@
+function addition(num) {
+	return num + 1;
+}
+
+const addition = (num) => {
+  return num + 1;
+}
+
+const addition = (num) => num + 1;
+
+const addition = function(num) {
+  return num + 1;
+}
+
+const addition = num => num + 1;
