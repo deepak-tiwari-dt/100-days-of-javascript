6059718009e4f8d3e584e2fad10290eb17eb02ea
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2026-04-24 08:18:56 +0530

    5th

---
+++ problem5.js
@@ -0,0 +1,13 @@
+function howManySeconds(hours) {
+  return hours * 60 * 60;
+}
+
+const howManySeconds = (hours) => {
+  return hours * 60 * 60;
+};
+
+const howManySeconds = (hours) => hours * 60 * 60;
+
+const howManySeconds = function (hours) {
+  return hours * 60 * 60;
+};
