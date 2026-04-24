8925f7184faccaa5f15051554478cb24a834a5d3
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2026-04-24 22:58:36 +0530

    6th

---
+++ problem6.js
@@ -0,0 +1,13 @@
+function triArea(base, height) {
+	return (base * height) / 2;
+}
+
+const triArea = (base, height) => {
+  return (base * height) / 2;
+}
+
+const triArea = function(base, height) {
+  return (base * height) / 2;
+}
+
+const triArea = (base, height) => (base * height) / 2;
