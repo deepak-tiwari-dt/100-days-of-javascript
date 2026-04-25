cc77a0c288c6a933dec153fd5e9ed7d350c48bd0
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2026-04-25 14:14:29 +0530

    7th

---
+++ problem7.js
@@ -0,0 +1,13 @@
+function calcAge(age) {
+	return age * 365;
+}
+
+const calcAge = (age) => {
+  return age * 365;
+}
+
+const calcAge = (age) => age * 365;
+
+const calcAge = function(age) {
+  return age * 365;
+}
