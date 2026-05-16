930b39ef06e7390c9d7a3ffbe0b0c3c56cb6aa9d
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2026-05-16 16:42:37 +0530

    25th

---
+++ problem25.js
@@ -0,0 +1,13 @@
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
+}
+
+const isPalindrome = (str) => {
+  return str === str.split("").reverse().join("");
+}
+
+const isPalindrome = function(str) {
+  return str === str.split("").reverse().join("");
+}
+
+const isPalindrome = (str) => str === str.split("").reverse().join("");
